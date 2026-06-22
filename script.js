4843eeb716763a353ca4f42182ce93139795193f
24173lh <24173lh@hvhs.school.nz>
2026-06-23 11:45:21 +1200

    starting

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 console.log("Hello world!")
+
+
+// Variables
+var burger = 0
+var chicken = 0
+var iceCream = 0
+var money = 0
+var name = ""
+const OUTPUT = document.getElementById("JavaScriptOutput");
+
+
+// Main Code
+
+
+
+
+
+
+
+// functions
+
+function addIceCream(){
+iceCream ++;
+localStorage.setItem("iceCream", iceCream);
+OUTPUT.innerHTML = "Ice Cream added to order"
+setTimeout(function() { OUTPUT.innerHTML = ""; }, 1000);
+}
+
+function addBurger(){
+burger ++;
+localStorage.setItem("burger", burger);
+OUTPUT.innerHTML = "Cheeseburger added to order"
+setTimeout(function() { OUTPUT.innerHTML = ""; }, 1000);
+}
+
+function addChicken(){
+chicken ++;
+localStorage.setItem("chicken", chicken);
+OUTPUT.innerHTML = "Fried Chicken added to order"
+setTimeout(function() { OUTPUT.innerHTML = ""; }, 1000);
+}
