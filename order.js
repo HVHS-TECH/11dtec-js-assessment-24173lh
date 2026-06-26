b2e6101c0da013e16eeda62b34bb0f0375b46dae
24173lh <24173lh@hvhs.school.nz>
2026-06-26 15:07:02 +1200

    locking in

--- order.js
+++ order.js
@@ -19,6 +19,13 @@ if (iceCream == null) { iceCream = 0; }
 
 var OUTPUT = document.getElementById("JavaScriptOutput");
 
+var AmountOfChicken
+
+var AmountOfBurgers
+
+var AmountOfIceCream
+
+
 total = iceCream*3 + burger*6 + chicken*5
 
 //Main Code
@@ -30,13 +37,36 @@ menuItems[1] + ": " + chicken + " ($" + chicken * 5 + ")" +
 "<br>" +
 menuItems[2] + ": " + iceCream + " ($" + iceCream * 3 + ")";OUTPUT.innerHTML += "<br><b>Total: </b>$" + total
 
+if (chicken>0) {
+  AmountChicken()} else AmountOfChicken = "";
+if (burger>0) {
+  AmountBurger() } else AmountOfBurgers = "";
+if (iceCream>0) {
+  AmountIceCream() } else AmountOfIceCream = "";
+
 
 //Functions
+
+function AmountChicken(){
+AmountOfChicken = "Chicken x" + chicken;
+return AmountOfChicken
+}
+
+function AmountBurger(){
+AmountOfBurgers = "Burger x" + burger;
+return AmountOfBurgers
+}
+
+function AmountIceCream(){
+AmountOfIceCream = "Ice Cream x" + iceCream;
+return AmountOfIceCream
+}
 
 function clearOrder() {
   localStorage.clear();
   OUTPUT.innerHTML = "Order cleared";
 }
+
 
 function getFormInput() {
   document.querySelector("#DetailsOutput").style.visibility = "visible";
@@ -45,10 +75,10 @@ function getFormInput() {
   var userMoney = Number(document.getElementById("moneyField").value);
   if (userMoney>0) {
     if (userMoney<total) {
-      detailsOutput.innerHTML = "Sorry, you can't afford this.";
+      detailsOutput.innerHTML = "You are a fat chud get a job you fatty.";
 
   } else {
-    detailsOutput.innerHTML = "<h1>Receipt:</h1> Name: " + userName + "<br>Money: $" + userMoney + "<br>Total: $" + total + "<br>Change: $" + (userMoney-total);
+    detailsOutput.innerHTML = "<h1>Receipt:</h1> Name: " + userName + "<br>Money: $" + userMoney + "<br>" + AmountOfChicken + "<br>" + AmountOfIceCream + "<br>" + AmountOfBurgers + "<br>Total: $" + total + "<br>Change: $" + (userMoney-total) + "<br>";
   }
   
 }
