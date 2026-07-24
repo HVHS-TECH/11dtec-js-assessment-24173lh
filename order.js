142ad6b1620e31b61efcb3696d5322ba0b54bac7
24173lh <24173lh@hvhs.school.nz>
2026-07-24 20:55:21 +1200

    final commit

--- order.js
+++ order.js
@@ -1,81 +1,107 @@
-document.querySelector("#DetailsOutput").style.visibility = 'hidden';
 
 // variables
 
+//menu items
 var menuItems = ["Cheeseburgers", "Fried Chicken", "Ice Creams"];
-var money = 0
-var name = ""
-var total 
 
-// getting items from script.js
-var burger = localStorage.getItem("burger");
-if (burger == null) { burger = 0; }
-var chicken = localStorage.getItem("chicken");
-if (chicken == null) { chicken = 0; }
-var iceCream = localStorage.getItem("iceCream");
-if (iceCream == null) { iceCream = 0; }
+//details
+var money = 0;
+var name = "";
 
+//total price
+var total;
+
+
+// getting items from script.js, if variable is empty, makes it 0
+
+//burger 
+var burger = localStorage.getItem("burger") || 0;
+
+//chicken
+var chicken = localStorage.getItem("chicken") || 0;
+
+//ice cream
+var iceCream = localStorage.getItem("iceCream") || 0;
+
+// Displaying order
 const OUTPUT = document.getElementById("JavaScriptOutput");
 
-var AmountOfChicken
-var AmountOfBurgers
-var AmountOfIceCream
 
-total = iceCream*3 + burger*6 + chicken*5
+//variables for receipt
+var AmountOfChicken;
+var AmountOfBurgers;
+var AmountOfIceCream;
 
-//Main Code
+// calculatig total
+total = iceCream*3 + burger*6 + chicken*5;
 
-OUTPUT.innerHTML =
 
-menuItems[1] + ": " + chicken + " ($" + chicken * 5 + ")" +
-"<br>" +
-menuItems[2] + ": " + iceCream + " ($" + iceCream * 3 + ")" + "<br>" + menuItems[0] + ": " + burger + " ($" + burger * 6 + ")"; 
-OUTPUT.innerHTML += "<br><b>Total: </b>$" + total
+// Main Code
 
+// Displaying order
+OUTPUT.innerHTML = menuItems[1] + ": " + chicken + " ($" + chicken * 5 + ")" + "<br>" + menuItems[2] + ": " + iceCream + " ($" + iceCream * 3 + ")" + "<br>" + menuItems[0] + ": " + burger + " ($" + burger * 6 + ")"; 
+
+// Displaying total
+OUTPUT.innerHTML += "<br><b>Total: </b>$" + total;
+
+
+// calculating whether to show items for receipt 
+// If there is more than 0 items on the order it will calculate the amount of items. 
+// Otherwise it will display nothing
 if (chicken>0) {
-  AmountChicken()} else AmountOfChicken = "";
+  AmountChicken();} 
+  else AmountOfChicken = "";
 if (burger>0) {
-  AmountBurger() } else AmountOfBurgers = "";
+  AmountBurger();} 
+  else AmountOfBurgers = "";
 if (iceCream>0) {
-  AmountIceCream() } else AmountOfIceCream = "";
+  AmountIceCream();} else 
+    AmountOfIceCream = "";
 
 
 //Functions
 
+//quantity of items
 function AmountChicken(){
 AmountOfChicken = "Chicken x" + chicken + "<br>";
-return AmountOfChicken
+return AmountOfChicken;
 }
 
 function AmountBurger(){
 AmountOfBurgers = "Burger x" + burger + "<br>";
-return AmountOfBurgers
+return AmountOfBurgers;
 }
 
 function AmountIceCream(){
 AmountOfIceCream = "Ice Cream x" + iceCream + "<br>";
-return AmountOfIceCream
+return AmountOfIceCream;
 }
 
+
+//clearing order
 function clearOrder() {
   localStorage.clear();
   OUTPUT.innerHTML = "Order cleared";
 }
 
-// getting results from form and displaying receipt/"not enough money"
+// getting results from form
 function getFormInput() {
-  document.querySelector("#DetailsOutput").style.visibility = "visible";
   var detailsOutput = document.getElementById("DetailsOutput");
   var userName = document.getElementById("nameField").value;
   var userMoney = Number(document.getElementById("moneyField").value);
-  if (userMoney>0) {
-    if (userMoney<total) {
-      detailsOutput.innerHTML = "Not enough money";
 
+// displaying receipt/"not enough money"
+if (userMoney<10000.1){
+  if (userMoney>-0.1){
+    //not enough money
+  if (userMoney<total) {
+      detailsOutput.innerHTML = "You need $" + (total - userMoney) + " more.";
+    //recipt
   } else {
     detailsOutput.innerHTML = "<h1>Receipt:</h1> Name: " + userName + "<br>Money: $" + userMoney + "<br>" + AmountOfChicken +  AmountOfIceCream + AmountOfBurgers + "<b>Total: </b>$" + total + "<br>Change: $" + (userMoney-total) + "<br>";
   }
-  
 }
+}
+
 
 }
--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // Variables
+
+// getting items from local storage
 var burger = Number(localStorage.getItem("burger")) || 0;
 var chicken = Number(localStorage.getItem("chicken")) || 0;
 var iceCream = Number(localStorage.getItem("iceCream")) || 0;
+
+
 const OUTPUT = document.getElementById("JavaScriptOutput");
+
+// background array and current background variable
 const images = ["images/burgerman.png","images/chickenman.png","images/icecreamwoman.png"];
 let current = 0;
 
@@ -17,6 +23,8 @@ setInterval(changeBackground, 5000);
 
 // functions
 
+
+// changing background
 function changeBackground() {
     document.body.style.backgroundImage = "url(" + images[current] + ")";
     current = (current + 1) % images.length;
@@ -24,26 +32,27 @@ function changeBackground() {
 
 // Adding items to local storage
 
+// Ice cream
 function addIceCream(){
-
 iceCream ++;
 localStorage.setItem("iceCream", iceCream);
-OUTPUT.innerHTML = "Ice Cream added to order"
+OUTPUT.innerHTML = "Ice Cream added to order";
 setTimeout(function() { OUTPUT.innerHTML = ""; }, 1000);
 }
 
-
-
+// Burger
 function addBurger(){
 burger ++;
 localStorage.setItem("burger", burger);
-OUTPUT.innerHTML = "Cheeseburger added to order"
+OUTPUT.innerHTML = "Cheeseburger added to order";
 setTimeout(function() { OUTPUT.innerHTML = ""; }, 1000);
 }
 
+// Chicken
 function addChicken(){
 chicken ++;
 localStorage.setItem("chicken", chicken);
-OUTPUT.innerHTML = "Fried Chicken added to order"
+OUTPUT.innerHTML = "Fried Chicken added to order";
 setTimeout(function() { OUTPUT.innerHTML = ""; }, 1000);
 }
+
